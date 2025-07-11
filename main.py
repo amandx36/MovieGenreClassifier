247fdbdedd6039038f8f15ded0330918de5d6c09
amandx36 <xamandeep360@gmail.com>
2025-07-11 10:24:57 +0530

    Start to push in the git

---
+++ main.py
@@ -0,0 +1,114 @@
+from text_changer import text_transformer
+import sys 
+import pickle 
+import string 
+import nltk 
+from nltk.stem.porter import PorterStemmer
+ps = PorterStemmer
+
+
+vectorizer = pickle.load(open('vectorization.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
+
+
+text_input = input("Enter the text to check , which genre it belongs to  >> ")
+transfor_text = [text_transformer(text_input)]
+
+vectorized_data = vectorizer.transform(transfor_text)
+predict_data = model.predict(vectorized_data)
+
+
+
+
+
+if predict_data[0] == 0 :
+    print ("action")
+
+
+elif predict_data[0] == 1 :
+    print ("adult")
+
+elif predict_data[0] == 2 :
+    print ("adventure")
+
+elif predict_data[0] == 3 :
+    print ("animation")
+
+elif predict_data[0] == 4 :
+    print ("biography")
+
+
+elif predict_data[0] == 5 :
+    print ("comedy")
+
+elif predict_data[0] == 6 :
+    print ("crime")
+
+elif predict_data[0] == 7 :
+    print ("documentary")
+
+elif predict_data[0] == 8 :
+    print ("drama")
+
+
+elif predict_data[0] == 9 :
+    print ("family")
+
+elif predict_data[0] == 10 :
+    print ("fantasy")
+
+elif predict_data[0] == 11 :
+    print ("game-show ")
+
+elif predict_data[0] == 12 :
+    print ("history ")
+
+
+elif predict_data[0] == 13 :
+    print ("horror")
+
+elif predict_data[0] == 14 :
+    print ("music")
+
+elif predict_data[0] == 15 :
+    print ("adult")
+
+elif predict_data[0] == 16 :
+    print ("mystery")
+
+
+elif predict_data[0] == 17 :
+    print ("news")
+
+elif predict_data[0] == 18 :
+    print ("reality-tv ")
+
+
+elif predict_data[0] == 19 :
+    print ("romance")
+
+elif predict_data[0] == 20 :
+    print ("sci-fi ")
+
+
+elif predict_data[0] == 21 :
+    print ("short")
+
+elif predict_data[0] == 22 :
+    print ("sport")
+
+elif predict_data[0] == 23 :
+    print ("talk-show")
+
+elif predict_data[0] == 24 :
+    print ("thriller ")
+
+
+elif predict_data[0] == 25 :
+    print ("war")
+
+elif predict_data[0] == 26 :
+    print ("fantasy")
+
+else :
+    print ("western")
---
+++ text_changer.py
@@ -0,0 +1,81 @@
+import nltk 
+from nltk.corpus import stopwords 
+from nltk.stem.porter import PorterStemmer
+
+import string 
+ps = PorterStemmer()
+
+
+#downloading the important libraries ! 
+nltk.download('punkt')
+nltk.download('stopwords')
+
+
+
+
+def text_transformer(text):
+
+     # a ) lower case conversion !!    
+    text  = text.lower()
+
+
+    #  b ) converting the sentence into the list of words 
+    text = nltk.word_tokenize(text)  #   (eg Aman deep loves oranges )  output :--> ['aman', 'deep', 'loves', 'oranges'] 
+
+
+
+    #  c ) now removing the special character using the .isalnum() and 
+
+    #store in the list after that you have to pass the list into the text 
+
+
+#    I/p :== "Aman deep!!!@  !!!a@@ loves oranges"   
+    l = []
+    for i in text :
+        if (i.isalnum()):
+            l.append(i)
+
+    text = l[:]
+   
+
+    # O/p :== ['aman', 'deep', 'a', 'loves', 'oranges']
+
+    l.clear()
+
+
+
+
+
+# d ) removing the puncutation and the stop words !! using the[[   string.punctuation and the stopwords.words('english')   ]] 
+
+    for i in text:
+        if i not in string.punctuation and i not in stopwords.words('english'):
+            l.append(i)
+
+
+    text = l[:]
+
+    l.clear()
+
+    
+# 3 ) convert the following  words in  its root form !! 
+    for i in text:
+        l.append(ps.stem(i))
+
+
+    text = l[:] 
+
+ 
+    l.clear()
+
+    # now the output is ['aman', 'deep', 'a', 'love', 'orang']
+
+    
+     # now the output will be [  ['aman', 'deep', 'love', 'orang'] ]
+    
+#    now remove the list and turn into the normal text yarrr !! using the   " ".join(argument)
+
+    return " ".join(text)
+    # o/p will be :==   aman deep love orang
+
+
